3af7b0b22afc93e582a7d944c10254d8a8a514c4
Fergus Leahy <fergus.leahy@gmail.com>
2014-11-13 17:09:21 +0000

    Improved HWDBClient

--- java/HWDBClient.java
+++ java/HWDBClient.java
@@ -7,34 +7,52 @@ import hwdb.*;
 import hwdb.Service;
 
 public class HWDBClient {
-private String radioTable = "create table radio (integer i, string s)";
-    public static void main(String[] args)
-    {
-        try {
-            String serviceName = "handler";
-            SRPC srpc = new SRPC();
-            service = srpc.offer(serviceName);
-            Connection conn = srpc.connect("localhost", 1234,"HWDB");
-            int port = srpc.details().getPort();
-            System.out.println(conn.call(String.format("SQL:select * from a 127.0.0.1 %d %s", port, serviceName)));
+  private String myServiceName = "handler";
+  private SRPC srpc;
+  private Connection conn;
+  private int incomingPort = 0;
 
-            } catch (Exception e) {
-                System.exit(1);
-            }
-
+  public HWDBClient(String addr, int port, String serviceName){
+    try {
+      srpc = new SRPC();
+      myServiceName = serviceName;
+      service = srpc.offer(myServiceName);
+      conn = srpc.connect(addr, port, "HWDB");
+      incomingPort = srpc.details().getPort();
+      System.out.println(conn.call(String.format("SQL:create table radio (coojaTime integer, id integer, event varchar(10))")));
+    } 
+    catch (Exception e) {
+      System.out.println("HWDB connection failed");
     }
-    static Service service;
-    public void run() {
-        try {
-            Message query;
-            while ((query = service.query()) != null) {
-                System.out.println(query.getContent());
-                query.getConnection().response("OK");
-            }
-        } catch (IOException e) {
-            System.exit(1);
-        }
+  }
+
+  public void insert(String stmt){
+    try {
+      System.out.println(conn.call(String.format("SQL:%s", stmt)));
+      System.out.println(conn.call(String.format("SQL:select * from radio")));
+    } 
+    catch (Exception e) {
+      System.out.println("SQL Insert failed");
     }
 
+    //System.out.println(conn.call(String.format("SQL:select * from radio 127.0.0.1 %d %s", incomingPort, myServiceName)));
+  }
+
+  static Service service;
+  public void run() {
+    try {
+      Message query;
+      while ((query = service.query()) != null) {
+        System.out.println(query.getContent());
+        query.getConnection().response("OK");
+      }
+    } catch (IOException e) {
+      System.exit(1);
+    }
+  }
+  public static void main(String[] args) {
+    HWDBClient hwdb = new HWDBClient("localhost", 1234, "COOJA");
+    hwdb.insert("insert into radio values ('394832904', '10', 'radio')");
+  }
 
 }
